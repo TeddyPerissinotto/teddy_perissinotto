aee8342ca586d402587184e8b1abac0bee51118f
teddy_perissinotto <teddy.perissinotto@lapiscine.pro>
2019-10-22 07:50:17 +0200

    22/10/19

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,6 +6,7 @@ namespace App\Controller;
 
 use App\Repository\EducationRepository;
 use App\Repository\ExperienceRepository;
+use App\Repository\ProjectRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,15 +16,17 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function homePage(ExperienceRepository $experienceRepository, EducationRepository $educationRepository)
+    public function homePage(ExperienceRepository $experienceRepository, EducationRepository $educationRepository, ProjectRepository $projectRepository)
     {
         $experiences = $experienceRepository->findAll();
         $educations = $educationRepository->findAll();
+        $projects = $projectRepository->findAll();
 
         return $this->render('homePage.html.twig',
             [
                 'experiences'=>$experiences,
-                'educations'=>$educations
+                'educations'=>$educations,
+                'projects'=>$projects
             ]);
     }
 }
